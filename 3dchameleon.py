f9a17d8db14af3c230382890467a0588f92d1ac8
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-04-07 17:24:38 -0400

    add ability to disable filament sensor

--- 3dchameleon.py
+++ 3dchameleon.py
@@ -8,16 +8,18 @@ class Chameleon:
         self.config = config
 
         self.filament_sensor_name = config.get('filament_sensor_name', 'fsensor')
-        self.filament_sensor0 = config.get('filament_sensor0', self.filament_sensor_name)
-        self.filament_sensor1 = config.get('filament_sensor1', self.filament_sensor_name)
-        self.filament_sensor2 = config.get('filament_sensor2', self.filament_sensor_name)
-        self.filament_sensor3 = config.get('filament_sensor3', self.filament_sensor_name)
-        self.filament_sensor_type = config.get('filament_sensor_type', 'filament_switch_sensor')
-        self.fsensor = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor_name}')
-        self.fsensor0 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor0}')
-        self.fsensor1 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor1}')
-        self.fsensor2 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor2}')
-        self.fsensor3 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor3}')
+        self.filament_sensor_enabled = self.filament_sensor_name != 'disabled'
+        if self.filament_sensor_enabled:
+            self.filament_sensor0 = config.get('filament_sensor0', self.filament_sensor_name)
+            self.filament_sensor1 = config.get('filament_sensor1', self.filament_sensor_name)
+            self.filament_sensor2 = config.get('filament_sensor2', self.filament_sensor_name)
+            self.filament_sensor3 = config.get('filament_sensor3', self.filament_sensor_name)
+            self.filament_sensor_type = config.get('filament_sensor_type', 'filament_switch_sensor')
+            self.fsensor = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor_name}')
+            self.fsensor0 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor0}')
+            self.fsensor1 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor1}')
+            self.fsensor2 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor2}')
+            self.fsensor3 = self.printer.load_object(self.config, f'{self.filament_sensor_type} {self.filament_sensor3}')
 
         self.pin = config.get('pin', '3dchameleon')
         self.chameleon_pin = self.printer.load_object(self.config, f'output_pin {self.pin}')
@@ -68,6 +70,12 @@ class Chameleon:
         )
 
         self.gcode.register_command(
+            'RESET_CHAMELEON',
+            self.cmd_RESET_CHAMELEON,
+            self.cmd_RESET_CHAMELEON_help
+        )
+
+        self.gcode.register_command(
             'QUERY_CHAMELEON_SENSOR',
             self.cmd_QUERY_CHAMELEON_SENSOR,
             self.cmd_QUERY_CHAMELEON_SENSOR_help
@@ -76,6 +84,8 @@ class Chameleon:
         self.printer.register_event_handler("klippy:ready", lambda: self.cmd_UPDATE_CHAMELEON_SENSOR(None))
     
     def _read_fsensor(self):
+        if not self.filament_sensor_enabled:
+            return
         return [self.fsensor0.runout_helper.filament_present, self.fsensor1.runout_helper.filament_present, self.fsensor2.runout_helper.filament_present, self.fsensor3.runout_helper.filament_present, self.fsensor.runout_helper.filament_present]
     
     def _set_chameleon(self, value):
@@ -84,40 +94,44 @@ class Chameleon:
             lambda print_time: self.chameleon_pin._set_pin(print_time, value))
     
     def cmd_UPDATE_CHAMELEON_SENSOR(self, gcmd=None):
+        if not self.filament_sensor_enabled:
+            return
         self.filament_detected = self._read_fsensor()
     
     cmd_UNLOAD_CHAMELEON_help = 'Unloads the 3DChameleon until the filament is past the filament runout sensor (when it reads False), and then waits unload_time to pull the filament out of the way for the next filament. Note that if the filament takes more than max_unload_time to trigger the filament sensor, then it will abort'
     def cmd_UNLOAD_CHAMELEON(self, gcmd):
         tool = gcmd.get_int('TOOL')
         self._set_chameleon(True)
-        start = time.time()
-        self.gcode.run_script_from_command('UPDATE_CHAMELEON_SENSOR')
-        while self.filament_detected[tool]:
-            logging.info(f'3DChameleon Unload Sensor: {self.filament_detected[tool]}')
+        if self.filament_sensor_enabled:
+            start = time.time()
             self.gcode.run_script_from_command('UPDATE_CHAMELEON_SENSOR')
-            if time.time() - start > self.max_unload_time:
-                self._set_chameleon(False)
-                self.gcode.run_script_from_command('M117 Unload Failed')
-                self.gcode.run_script_from_command('PAUSE')
-                break
-            self.gcode.run_script_from_command('G4 P250')
+            while self.filament_detected[tool]:
+                logging.info(f'3DChameleon Unload Sensor: {self.filament_detected[tool]}')
+                self.gcode.run_script_from_command('UPDATE_CHAMELEON_SENSOR')
+                if time.time() - start > self.max_unload_time:
+                    self._set_chameleon(False)
+                    self.gcode.run_script_from_command('M117 Unload Failed')
+                    self.gcode.run_script_from_command('PAUSE')
+                    break
+                self.gcode.run_script_from_command('G4 P250')
         self.gcode.run_script_from_command(f'G4 P{self.unload_time * 1000}')
         self._set_chameleon(False)
     
     cmd_LOAD_CHAMELEON_help = 'Loads the 3DChameleon until the filament is past the filament runout sensor (when it reads True), and then waits load_time to push the filament into the extruder. Note that if the filament takes more than max_load_time to trigger the filament sensor, then it will abort'
     def cmd_LOAD_CHAMELEON(self, gcmd):
         self._set_chameleon(True)
-        start = time.time()
-        self.gcode.run_script_from_command('UPDATE_CHAMELEON_SENSOR')
-        while not self.filament_detected:
-            logging.info(f'3DChameleon Load Sensor: {self.filament_detected}')
+        if self.filament_sensor_enabled:
+            start = time.time()
             self.gcode.run_script_from_command('UPDATE_CHAMELEON_SENSOR')
-            if time.time() - start > self.max_load_time:
-                self._set_chameleon(False)
-                self.gcode.run_script_from_command('M117 Load Failed')
-                self.gcode.run_script_from_command('PAUSE')
-                break
-            self.gcode.run_script_from_command('G4 P250')
+            while not self.filament_detected:
+                logging.info(f'3DChameleon Load Sensor: {self.filament_detected}')
+                self.gcode.run_script_from_command('UPDATE_CHAMELEON_SENSOR')
+                if time.time() - start > self.max_load_time:
+                    self._set_chameleon(False)
+                    self.gcode.run_script_from_command('M117 Load Failed')
+                    self.gcode.run_script_from_command('PAUSE')
+                    break
+                self.gcode.run_script_from_command('G4 P250')
         self.gcode.run_script_from_command(f'G4 P{self.load_time * 1000}')
         self._set_chameleon(False)
     
@@ -128,6 +142,13 @@ class Chameleon:
         pulses_ms = int(pulses * self.pulse_time * 1000)
         self.gcode.run_script_from_command(f'G4 P{pulses_ms}')
         self._set_chameleon(False)
+    
+    cmd_RESET_CHAMELEON_help = 'Resets the 3DChameleon\'s state by rapidly pulsing it twice'
+    def cmd_RESET_CHAMELEON(self, gcmd):
+        for _ in range(2):
+            self._set_chameleon(True)
+            self._set_chameleon(False)
+        gcmd.respond_info('Reset Chameleon State')
     
     cmd_PRESS_CHAMELEON_help = 'Press the 3DChameleon for the duration of DURATION'
     def cmd_PRESS_CHAMELEON(self, gcmd):
@@ -145,6 +166,8 @@ class Chameleon:
     
     cmd_QUERY_CHAMELEON_SENSOR_help = 'Returns the current state of the 3DChameleon filament sensor'
     def cmd_QUERY_CHAMELEON_SENSOR(self, gcmd):
+        if not self.filament_sensor_enabled:
+            return
         value = self._read_fsensor()
         gcmd.respond_info(f'Sensor value: {value}')
 
