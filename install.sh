b3cda3c7913cb0dfc302fbc86f0004182cc20179
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-04-09 15:30:40 -0400

    Update install.sh and update.sh
    Added feedback with echo and added a chebang so that the script can be executed directly

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 cp 3dchameleon.cfg ~/printer_data/config/3dchameleon.cfg
+echo Installed Configuration
 echo "[include 3dchameleon.cfg]" >> ~/printer_data/config/printer.cfg
+echo Added Configuration to printer.cfg
 sh ./update.sh
+echo Installed 3dchameleon.py
